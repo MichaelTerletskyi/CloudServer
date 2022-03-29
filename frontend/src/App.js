4269bdefbb0817bb4d1be5f920b2189dc3b115ea
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2022-03-30 01:03:25 +0300

    Contact us pages has been created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,8 +9,6 @@ import {LogOut} from "./components/Pages/LogOut";
 import {LogIn} from "./components/Pages/LogIn";
 import {Register} from "./components/Pages/Register";
 
-// https://react-select.com/home
-
 const App = () => {
     return (
         <>
--- frontend/src/components/Pages/Contact.js
+++ frontend/src/components/Pages/Contact.js
@@ -1,9 +1,13 @@
 import React from "react";
+import {SocialIcon} from 'react-social-icons';
 
+// TODO
 export const Contact = () => {
     return (
         <div>
-            <h1>Contact Us</h1>
+            <SocialIcon url="https://twitter.com/#"/>
+            <SocialIcon url="https://github.com/MichaelTerletskyi"/>
+            <SocialIcon url="https://www.linkedin.com/in/michael-terletskyi-7a3a951a5/"/>
         </div>
     );
 };
