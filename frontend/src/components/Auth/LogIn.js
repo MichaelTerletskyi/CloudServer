8ff9ef9b48452fe6ae3de866591602323570b8dd
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2022-04-26 09:49:33 +0300

    Login page form validation has been added

--- frontend/src/components/Auth/LogIn.js
+++ frontend/src/components/Auth/LogIn.js
@@ -17,18 +17,7 @@ import "./vendor/daterangepicker/daterangepicker.css";
 import "./css/util.css";
 import "./css/main.css";
 
-
 export const LogIn = (props) => {
-    const required = (value) => {
-        if (!value) {
-            return (
-                <div className="alert alert-danger" role="alert">
-                    This field is required!
-                </div>
-            );
-        }
-    };
-
     const form = useRef();
     const checkBtn = useRef();
 
@@ -83,20 +72,23 @@ export const LogIn = (props) => {
                         <Form className="login100-form validate-form" onSubmit={handleLogin} ref={form}>
                             <span className="login100-form-title p-b-49">Login</span>
 
+
                             <div className="wrap-input100 validate-input m-b-23" data-validate="Username is required">
                                 <span className="label-input100">Username</span>
                                 <input className="input100"
                                        type="text"
                                        name="username"
                                        placeholder="Type your username"
-                                       required={true}
-                                       minLength={2}
-                                       maxLength={32}
                                        value={username}
                                        onChange={onChangeUsername}
                                 />
                                 <span className="focus-input100" data-symbol="&#xf206;"/>
                             </div>
+
+                            <div role="alert" className={"alert-msg"}>
+                                <a>&nbsp;{message && (username.length < 2 || username.length > 32) ? "Minimum 2 characters are required" : ""}</a>
+                            </div>
+
 
                             <div className="wrap-input100 validate-input" data-validate="Password is required">
                                 <span className="label-input100">Password</span>
@@ -104,14 +96,16 @@ export const LogIn = (props) => {
                                        type="password"
                                        name="pass"
                                        placeholder="Type your password"
-                                       required={true}
-                                       minLength={8}
-                                       maxLength={32}
                                        value={password}
                                        onChange={onChangePassword}
                                 />
                                 <span className="focus-input100" data-symbol="&#xf190;"/>
                             </div>
+
+                            <div role="alert" className={"alert-msg"}>
+                                <a>&nbsp;{message && (password.length < 8 || password.length > 32) ? "Minimum 8 characters are required" : ""}</a>
+                            </div>
+
 
                             <div className="text-right p-t-8 p-b-31">
                                 <a href="#">Forgot password?</a>
@@ -120,21 +114,19 @@ export const LogIn = (props) => {
                             <div className="container-login100-form-btn">
                                 <div className="wrap-login100-form-btn">
                                     <div className="login100-form-bgbtn"/>
-                                    <button type="submit" className="login100-form-btn" disabled={loading}>Sign in</button>
+                                    <button type="submit" className="login100-form-btn" disabled={loading}>Sign in
+                                    </button>
                                 </div>
                             </div>
 
-                            {message && (
-                                <div className="form-group">
-                                    <div className={successful ? "alert alert-success" : "alert alert-danger"} role="alert">
-                                        {message}
-                                    </div>
-                                </div>
-                            )}
+                            <div role="alert" className={"alert-msg"}>
+                                <a>&nbsp;{message}</a>
+                            </div>
 
                             <CheckButton style={{display: "none"}} ref={checkBtn}/>
 
-                            <div className="txt1 text-center p-t-54 p-b-20">
+
+                            <div className="txt1 text-center p-t-25 p-b-20">
                                 <span>Or Sign Up Using</span>
                             </div>
 
@@ -150,7 +142,8 @@ export const LogIn = (props) => {
                                 </a>
                             </div>
 
-                            <div className="flex-col-c p-t-155">
+
+                            <div className="flex-col-c p-t-25">
                                 <span className="txt1 p-b-17">Or Sign Up Using</span>
                                 <a href={"/register"} className="txt2">Sign Up</a>
                             </div>
